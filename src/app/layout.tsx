d734e917b7d3591e4de5c2b5075513c2cf88ee57
dariomnalerio <dariomnalerio@gmail.com>
2024-04-16 03:43:12 -0300

    add env schema config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { env } from "@/config";
 
 const fontSans = FontSans({
   subsets: ["latin"],
