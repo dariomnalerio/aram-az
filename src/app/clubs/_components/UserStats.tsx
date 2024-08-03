d7548524ec327e3ca096e390a61f4ab8398b6e48
dariomnalerio <dariomnalerio@gmail.com>
2024-08-03 17:11:41 -0300

    fix club view progress bar

--- src/app/clubs/_components/UserStats.tsx
+++ src/app/clubs/_components/UserStats.tsx
@@ -7,6 +7,7 @@ type Props = {
   champCount: number;
 };
 export function UserStats({ member, champCount }: Props) {
+  const completedPercentage = (member.champions.length / champCount) * 100;
   return (
     <>
       <div className='flex justify-evenly gap-2 text-sm mb-1 my-2'>
@@ -15,7 +16,7 @@ export function UserStats({ member, champCount }: Props) {
         </span>
         <span>{Math.round((member.champions.length / champCount!) * 100)}%</span>
       </div>
-      <Progress value={member.champions.length} max={champCount} />
+      <Progress value={completedPercentage} max={100} />
     </>
   );
 }
