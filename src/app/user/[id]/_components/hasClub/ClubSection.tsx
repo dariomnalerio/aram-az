8d7d426212935eb33c9a93364fdeac209a4f6726
dariomnalerio <dariomnalerio@gmail.com>
2024-08-03 15:24:47 -0300

    refactor: mobile view

--- src/app/user/[id]/_components/hasClub/ClubSection.tsx
+++ src/app/user/[id]/_components/hasClub/ClubSection.tsx
@@ -43,13 +43,11 @@ export function ClubSection({ options }: ClubSectionType) {
 
   return (
     <div className='w-full max-w-[288px] sm:max-w-[576px] md:max-w-[648px] lg:max-w-[720px] xl:max-w-[936px] 2xl:max-w-[1080px] flex flex-col'>
-      <div className='flex justify-between'>
-        <SelectClub
-          options={options}
-          onChange={onChange}
-          defaultValue={searchParams.get("club") && searchParams.get("club")}
-        />
-      </div>
+      <SelectClub
+        options={options}
+        onChange={onChange}
+        defaultValue={searchParams.get("club") && searchParams.get("club")}
+      />
       <ChallengeSection clubId={value} />
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,13 @@ import Link from "next/link";
 import Image from "next/image";
 import { Separator } from "../ui/separator";
 import { getUser, handleLogout } from "@/app/actions/";
+import { AlignJustify } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 export default async function Header() {
   const { user } = await getUser();
@@ -13,14 +20,15 @@ export default async function Header() {
     <header>
       <nav className='flex items-center justify-between px-3 py-5 shadow-lg sm:px-5 md:px-10'>
         <div>
-          <Link href='/' className='flex sm:gap-2 md:gap-4'>
+          <Link href='/' className='flex gap-1 sm:gap-2 md:gap-4'>
             <div className='relative h-4 w-4'>
-              <Image src={logo} alt='ARAM-AZ Logo' className='hidden sm:flex' />
+              <Image src={logo} alt='ARAM-AZ Logo' className='flex' />
             </div>
             <span className='md:text-md text-xs font-semibold'>ARAM-AZ</span>
           </Link>
         </div>
-        <div className='flex items-center gap-3 md:gap-8'>
+
+        <div className='hidden sm:flex items-center gap-3 md:gap-8'>
           <div className='hidden gap-9 min-[600px]:flex'>
             {!user && (
               <Link href='#' className='hover:underline underline-offset-4'>
@@ -55,6 +63,54 @@ export default async function Header() {
               </form>
             </>
           )}
+        </div>
+
+        {/* Mobile hamburger menu*/}
+        <div className='sm:hidden'>
+          <DropdownMenu dir='ltr'>
+            <DropdownMenuTrigger asChild>
+              <Button variant={"outline"}>
+                <AlignJustify />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='flex flex-col items-center gap-1 bg-background p-4'>
+              {!user && (
+                <DropdownMenuItem>
+                  <Link href='#'>Demo</Link>
+                </DropdownMenuItem>
+              )}
+
+              {!user && (
+                <DropdownMenuItem>
+                  <Link href={`/login`}>Log in</Link>
+                </DropdownMenuItem>
+              )}
+
+              {user && (
+                <>
+                  <DropdownMenuItem>
+                    <Button asChild variant={"outline"} size={"sm"} className='text-sm w-20'>
+                      <Link href='/clubs'>Clubs</Link>
+                    </Button>
+                  </DropdownMenuItem>
+
+                  <DropdownMenuItem>
+                    <Button size={"sm"} className='text-sm w-20' asChild>
+                      <Link href={`/user/${user.id}`}>Profile</Link>
+                    </Button>
+                  </DropdownMenuItem>
+
+                  <DropdownMenuItem>
+                    <form action={handleLogout}>
+                      <Button variant={"destructive"} size={"sm"} className='text-sm w-20'>
+                        Log out
+                      </Button>
+                    </form>
+                  </DropdownMenuItem>
+                </>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </nav>
       <Separator className='h-[0.1px] w-full bg-primary/20' />
